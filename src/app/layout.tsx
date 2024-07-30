8569efe4955e8580a9f0e9dbc66bffa4fb37bf83
Ibrahim Habib <ibrahimhabib.eg@gmail.com>
2024-07-30 18:01:46 +0300

    Add Google Tag Manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { GoogleTagManager } from '@next/third-parties/google'
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <GoogleTagManager gtmId="G-SVLGQ72ZN4"/>
     </html>
   );
 }
